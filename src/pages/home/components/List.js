b13226e1ed1556b90b3dd1dfc43da2ca2a82dfe0
MLuminary <418037025@qq.com>
2018-11-04 18:07:09 +0800

    List 组件

--- src/pages/home/components/List.js
+++ src/pages/home/components/List.js
@@ -1,13 +1,36 @@
 import React , {Component} from 'react'
+import { connect } from 'react-redux'
+import { ListItem, ListInfo } from '../style'
 
 class List extends Component {
 
 	render() {
+		const { list } = this.props
 		return (
-			<div>List</div>
+			<div>
+				{
+					list.map(item => {
+						return (
+							<ListItem key={item.get('id')}>
+								<img className='pic' src={item.get('url')} alt="pic"/>
+								<ListInfo>
+									<h3 className='title'>{item.get('title')}</h3>
+									<p className='desc'>{item.get('desc')}</p>
+								</ListInfo>
+							</ListItem>
+						)
+					})
+				}
+			</div>
 		)
 	}
 
 }
 
-export default List
+const mapStateToProps = (state) => {
+	return {
+		list: state.getIn(['home','articleList'])
+	}
+}
+
+export default connect(mapStateToProps, null)(List)
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -32,6 +32,32 @@ const defaultState = fromJS({
 			url: '//upload.jianshu.io/collections/images/21/20120316041115481.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/64/h/64',
 			txt: '简书电影'
 		}
+	],
+	articleList: [
+		{
+			id: 1,
+			url: '//upload-images.jianshu.io/upload_images/12100388-5a38510faf287b38?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+			title: '35 平开间，这样装修谁舍得出租',
+			desc: '建面 35 平开间公寓，一个卫生间和阳台是单独的，其他区域大开间。阳台做了厨房还得兼顾晾衣服。开间兼顾卧室和会客功能 进门右手就是卫生间，虽小但是够...'
+		},
+		{
+			id: 2,
+			url: '//upload-images.jianshu.io/upload_images/12100388-5a38510faf287b38?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+			title: '35 平开间，这样装修谁舍得出租',
+			desc: '建面 35 平开间公寓，一个卫生间和阳台是单独的，其他区域大开间。阳台做了厨房还得兼顾晾衣服。开间兼顾卧室和会客功能 进门右手就是卫生间，虽小但是够...'
+		},
+		{
+			id: 3,
+			url: '//upload-images.jianshu.io/upload_images/12100388-5a38510faf287b38?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+			title: '35 平开间，这样装修谁舍得出租',
+			desc: '建面 35 平开间公寓，一个卫生间和阳台是单独的，其他区域大开间。阳台做了厨房还得兼顾晾衣服。开间兼顾卧室和会客功能 进门右手就是卫生间，虽小但是够...'
+		},
+		{
+			id: 4,
+			url: '//upload-images.jianshu.io/upload_images/12100388-5a38510faf287b38?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240',
+			title: '35 平开间，这样装修谁舍得出租',
+			desc: '建面 35 平开间公寓，一个卫生间和阳台是单独的，其他区域大开间。阳台做了厨房还得兼顾晾衣服。开间兼顾卧室和会客功能 进门右手就是卫生间，虽小但是够...'
+		}
 	]
 })
 
